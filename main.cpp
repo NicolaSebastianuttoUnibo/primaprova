733d2ad7ea565ff8825a89cfdc8822c21344f680
xXAlbanoXx <132943685+xXAlbanoXx@users.noreply.github.com>
2023-10-26 11:23:04 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,15 @@
-#include <iostream>
 #include <array>
 #include "ResonanceType.hpp"
 
 int main(){
     ParticleType* particleArray[2];
-    ParticleType part;
-    ResonanceType reso;
-    std::cin >> part;
-    std::cin >> reso;
+    char name1 = 'A';
+    char name2 = 'B';
+    ParticleType part(&name1, 2., 3);
+    ResonanceType reso(&name2, 4., 5, 6.);
     particleArray[0] = &part;
     particleArray[1] = &reso;
     for (int i{0}; i < 1; i++){
-        particleArray[i].Print();
+        (*particleArray[i]).Print();
     }
 }
