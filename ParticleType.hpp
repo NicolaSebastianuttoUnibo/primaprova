1c8bc7fe654b5decee2a4c5a4d8ed679f59a889a
xXAlbanoXx <132943685+xXAlbanoXx@users.noreply.github.com>
2023-10-26 12:03:32 +0200

    Update ParticleType.hpp

--- ParticleType.hpp
+++ ParticleType.hpp
@@ -14,7 +14,7 @@ double GetMass();
 int GetCharge();
 void Print();
 bool operator==(const ParticleType lhs, const ParticleType rhs){
-  return lhs.isEqual( rhs ) && lhs.fName == rhs.fName && lhs.fMass == rhs.fMass && lhs.fNCharge == rhs.fCharge;
+  return lhs.isEqual( rhs ) && lhs.fName == rhs.fName && lhs.fMass == rhs.fMass && lhs.fCharge == rhs.fCharge;
 }
 
 ParticleType(char* name, double mass, int charge);
