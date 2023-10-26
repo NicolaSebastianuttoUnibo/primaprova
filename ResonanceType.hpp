e4888b4385922df0642e17fa9ca60097d4cbcd27
xXAlbanoXx <132943685+xXAlbanoXx@users.noreply.github.com>
2023-10-26 12:03:13 +0200

    Update ResonanceType.hpp

--- ResonanceType.hpp
+++ ResonanceType.hpp
@@ -12,6 +12,10 @@ public:
   double getfWidth() const;
   void Print();
 
+bool operator==(const ParticleType lhs, const ParticleType rhs){
+  return lhs.isEqual( rhs ) && lhs.fName == rhs.fName && lhs.fMass == rhs.fMass && lhs.fCharge == rhs.fCharge && lhs.fWidth == rhs.fWidth;
+}
+
   private:
     double const fWidth;
 
