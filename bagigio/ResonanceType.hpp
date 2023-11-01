0b5c030cc4f8e81e4d5236ee3fb41f6f45f7131b
EdoardoMassari <81566105+EdoardoMassari@users.noreply.github.com>
2023-11-01 18:35:21 +0100

    Update ResonanceType.hpp

--- bagigio/ResonanceType.hpp
+++ bagigio/ResonanceType.hpp
@@ -2,7 +2,7 @@
 #define RESONANCETYPE_HPP
 
 #include "iostream"
-#include "particleType.hpp"
+#include "ParticleType.hpp"
 
 class ResonanceType : public ParticleType
 {
@@ -10,7 +10,7 @@ public:
   ResonanceType(const char*,const double, const int , const double);
   
   const double getfWidth() const;
-  void Print();
+   void Print();
 
 
 
@@ -20,3 +20,13 @@ public:
 };
 
 #endif
+
+/*
+
+InvMass(Particle &p)
+{
+  double Minv= std::sqrt((this.GetEnergy+p.GetEnergy)^2-(std::sqrt(this.GetPx+p.GetPx)^2+std::sqrt(this.GetPy+p.GetPy)^2 + std::sqrt(this.GetPz+p.GetPz)^2)^2)
+}
+
+
+*/
