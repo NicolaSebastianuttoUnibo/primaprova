322baa844646353801c5b5fc12ac220bc35da79f
EdoardoMassari <81566105+EdoardoMassari@users.noreply.github.com>
2023-11-09 09:39:58 +0100

    Add files via upload

---
+++ vercingetorige - Copia/Particle_cpp.d
@@ -0,0 +1,6 @@
+
+# DO NOT DELETE
+
+./Particle_cpp.so: Particle.hpp ResonanceType.hpp ParticleType.hpp
+./Particle_cpp.so: /home/massa/root/include/RVersion.h /home/massa/root/include/RConfig.h /home/massa/root/include/TClass.h /home/massa/root/include/TDictAttributeMap.h /home/massa/root/include/TInterpreter.h /home/massa/root/include/TROOT.h /home/massa/root/include/TBuffer.h /home/massa/root/include/TMemberInspector.h /home/massa/root/include/TError.h /home/massa/root/include/RtypesImp.h /home/massa/root/include/TIsAProxy.h /home/massa/root/include/TFileMergeInfo.h /home/massa/root/include/TCollectionProxyInfo.h /home/massa/root/bin/rootcling
+Particle_cpp__ROOTBUILDVERSION= 6.26/06
---
+++ vercingetorige - Copia/mmm.C
@@ -0,0 +1,47 @@
+#include "Particle.hpp"
+void mmm(){
+//gRandom->SetSeed();
+
+Particle p("ci",5,5,5);
+
+
+ParticleType pip("pione +",4,5);
+ParticleType pim("pione -",4,5);
+ParticleType kap("kaone +",4,5);
+ParticleType kam("kaone -",4,5);
+ParticleType prp("protone +",4,5);
+ParticleType prm("protone -",4,5);
+ResonanceType r1("risonanza",4,5,6);
+ResonanceType r2("risonanza34234",466666,5,6);
+
+
+ /* p.AddParticleType("risonanza",4,5,6);
+p.PrintAll();  */
+
+Particle::AddParticleType( "risonanza",4,5,6);
+/*Particle::AddParticleType( "protone -",4,5,0);
+Particle::AddParticleType( "protone -",4,5,0);
+Particle::AddParticleType( "protone -",4,5,0);
+Particle::AddParticleType( "risonanza",4,5,6);*/
+Particle::PrintAll(); 
+//p.PrintParticle(); 
+//Particle::o(pip);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+std::cout <<"\n er brother";
+
+
+std::cout<<"\ncifuyguygao\n";
+}
